2b9a735d9e78a4021974c57c4358c045940f5bd8
Larissa <larissa.queiroz@nepen.org.br>
2022-08-10 10:13:01 -0300

    feat: criação do model e controller Filme e primeira rota get

--- Filmes_API/Filmes_API/Controllers/FilmeController.cs
+++ Filmes_API/Filmes_API/Controllers/FilmeController.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Filmes_API.Models;
 
 namespace Filmes_API.Controllers
 {
-    public class FilmeController : Controller
+    [Route("api/filmes")]
+    [ApiController]
+    public class FilmeController : ControllerBase
     {
-        public IActionResult Index()
+        private static List<Filme> filmes = new List<Filme>
         {
-            return View();
+            new Filme
+            {
+                Id = 1,
+                titulo = "Brilho Eterno de Uma Mente Sem Lembranças",
+                ano = 2004,
+                duracao = 108,
+                sinopse = "Quando sua relaçao se volta complicada, um casal tenta um procedimento " +
+                "médico para que suas memorias sejam apagadas.",
+                avaliacao_geral = 0,
+                classif_indicativa = "14",
+                elenco = new string[] { "Jim Carrey", "Kate Winslet", "Tom Wilkinson"}
+            },
+
+            new Filme
+            {
+                Id = 2,
+                titulo = "Waking Life",
+                ano = 2001,
+                duracao = 99,
+                sinopse = "Um homem constrói um sonho ao encontrar várias pessoas e discutir os significados " +
+                "e propósitos do universo.",
+                avaliacao_geral = 0,
+                classif_indicativa = "Livre",
+                elenco = new string[] { "Ethan Hawke", "Trevor Jake Brooks", "Lorelei Linklater"}
+            },
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Filme>>> Get()
+        {
+            return Ok(filmes);
         }
     }
 }
--- Filmes_API/Filmes_API/Models/Filme.cs
+++ Filmes_API/Filmes_API/Models/Filme.cs
@@ -2,5 +2,17 @@
 {
     public class Filme
     {
+        public int Id { get; set; }
+        
+
+
+        public string titulo { get; set; } = string.Empty;
+        public int ano { get; set; }
+        public int duracao { get; set; }
+        public string sinopse { get; set; } = string.Empty;
+        public float avaliacao_geral { get; set; }
+        public string classif_indicativa { get; set; } = string.Empty;
+        public string[] elenco { get; set; } = new string[3];
+
     }
 }
